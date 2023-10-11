11d65bbe6604bf638687456eb8aa3636b7d2e9b8
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-11 15:20:42 +0200

    Add project files.

---
+++ 1. Tell my age/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Tell_my_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter my name");
+                
+                Console.ReadLine();
+
+            Console.WriteLine("Enter the year I was born");
+            int birthYear = Convert.ToInt32(Console.ReadLine());    
+
+            int CurrentYear = DateTime.Now.Year;
+            int age = CurrentYear - birthYear;
+            Console.WriteLine(2023 - 2000);
+
+            Console.ReadLine() ;
+
+
+        }
+    }
+}
